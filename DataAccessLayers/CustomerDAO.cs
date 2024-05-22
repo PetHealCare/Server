7863c49a2f707444d12f34653745c7e939b37114
Trọng Phan <dev.sytrong@gmail.com>
2024-05-22 15:08:14 +0700

    Add CustomerDAO

---
+++ DataAccessLayers/CustomerDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects.Models;
+
+namespace DataAccessLayers
+{
+	public class CustomerDAO : GenericDAO<Customer>
+	{
+		private static readonly Lazy<CustomerDAO> _instance =
+		new Lazy<CustomerDAO>(() => new CustomerDAO(new PetHealthCareContext()));
+		public static CustomerDAO Instance => _instance.Value;
+		public CustomerDAO(PetHealthCareContext context) : base(context)
+		{
+		}
+	}
+}
