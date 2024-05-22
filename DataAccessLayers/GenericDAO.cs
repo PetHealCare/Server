3b87a79dae6f7af6e842320957663f3324df6017
Trọng Phan <dev.sytrong@gmail.com>
2024-05-22 14:58:57 +0700

    Add generic dao And class lib DTOs

---
+++ DataAccessLayers/GenericDAO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayers
+{
+	public class GenericDAO<T> : IGenericDAO<T> where T : class
+	{
+		private readonly PetHealthCareContext _context;
+
+		public GenericDAO(PetHealthCareContext context)
+		{
+			_context = context;
+		}
+
+		public bool Create(T entity)
+		{
+			_context.Set<T>().Add(entity);
+			return _context.SaveChanges() > 0;
+		}
+
+		public T GetById(int id)
+		{
+			return _context.Set<T>().Find(id);
+		}
+
+		public List<T> GetAll()
+		{
+			return _context.Set<T>().ToList();
+		}
+
+		public bool Update(T entity)
+		{
+			_context.Set<T>().Attach(entity);
+			_context.Entry(entity).State = EntityState.Modified;
+			return _context.SaveChanges() > 0;
+		}
+
+		public bool Delete(int id)
+		{
+			var entity = _context.Set<T>().Find(id);
+			if (entity == null)
+			{
+				return false;
+			}
+			_context.Set<T>().Remove(entity);
+			return _context.SaveChanges() > 0;
+		}
+	}
+
+}
