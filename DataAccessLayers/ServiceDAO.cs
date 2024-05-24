d6c7f2cee4d61e780b2d753b03ec323f96d60c2a
Kha Cao <kha15cm@gmail.com>
2024-05-24 18:51:30 +0700

    feat: Get List Service

---
+++ DataAccessLayers/ServiceDAO.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayers
+{
+    public class ServiceDAO : GenericDAO<Service>
+    {
+        private static readonly Lazy<ServiceDAO> _instance =
+        new Lazy<ServiceDAO>(() => new ServiceDAO(new PetHealthCareContext()));
+        public static ServiceDAO Instance => _instance.Value;
+        public ServiceDAO(PetHealthCareContext context) : base(context)
+        {
+
+        }
+    }
+}
