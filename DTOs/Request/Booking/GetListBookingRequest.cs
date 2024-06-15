33937bed46210b4574708e7af5f2f041735549f3
Kha Cao <kha15cm@gmail.com>
2024-06-15 10:17:17 +0700

    fix: getList

---
+++ DTOs/Request/Booking/GetListBookingRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Request.Booking
+{
+    public class GetListBookingRequest
+    {
+       
+        public int PetId { get; set; }
+        public int CustomerId { get; set; }
+        public int DoctorId { get; set; }
+        public int ScheduleId { get; set; }
+        public byte? Slot { get; set; }
+        public bool? Status { get; set; }
+    }
+}
